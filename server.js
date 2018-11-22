7d1047dcb8ece9e9a0bcc8151b88fb402f24462c
Hariprakash1997 <1997hariprakash@gmail.com>
2018-11-22 13:03:51 +0530

    middleware

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -13,6 +13,24 @@ hbs.registerHelper('getCurrentYear', () => {
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
    res.render('home.hbs', {
